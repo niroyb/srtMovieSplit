e677e7732a1d58c15940f32d80be875cfe345283
niroyb <niroyb@gmail.com>
2014-05-12 01:14:13 -0400

    remove global vars, use example data

--- srt_freq.py
+++ srt_freq.py
@@ -2,23 +2,27 @@ import pysrt
 import re
 from collections import Counter
 
-SRT_FILE = 'Example.srt'
-
 def fix_capital(string):
     if string.isupper():
         return string
     return string.lower()
 
-def main():
-    subs = pysrt.open(SRT_FILE)
+def get_word_counts(srt_path):
+    subs = pysrt.open(srt_path)
     freq = Counter()
     for s in subs:
         words = map(fix_capital, re.findall('\w+', s.text))
         freq.update(words)
-        #print words
-    for word, count in freq.most_common():
-        if len(word) > 2 and count > 5:
-            print count, word
-    pass
+    return freq
 
-main()
+def print_word_counts(word_counts):
+    for word, count in word_counts.most_common():
+        if len(word) > 2:
+            print count, word
+
+def analyse_srt(srt_path):
+    counts = get_word_counts(srt_path)
+    print_word_counts(counts)
+
+if __name__ == '__main__':
+    analyse_srt('DemoData/sintel_en.srt')
--- srt_movie_split.py
+++ srt_movie_split.py
@@ -4,46 +4,19 @@ import subprocess
 import datetime
 import os
 
-VID_FILE = 'Example.avi'
-SRT_FILE = 'Example.srt'
-USE_REENCODING = True
+def srt_movie_split(path_video, path_subtitle, prefix, pattern, use_reencoding=True):
+    all_subs = pysrt.open(path_subtitle)
+    subs_to_extract = filter_subs(all_subs, pattern)
+    print len(subs_to_extract), 'parts to extract'
+    if len(subs_to_extract) is 0:
+        return
+    extension = get_extension(path_video)
+    clip_names = get_clip_names(subs_to_extract, extension, prefix)
+    extract_clips(path_video, subs_to_extract, clip_names, use_reencoding)
 
-def get_subs():
-    subs = pysrt.open(SRT_FILE)
-    return subs
-
-def filter_subs(subs, pattern):
-    ret = []
-    for sub in subs:
-        if re.search(pattern, sub.text, re.MULTILINE):
-            ret.append(sub)
-    return ret
-
-def time_to_timedelta(time):
-    return datetime.timedelta(hours=time.hour, minutes=time.minute, seconds=time.second, milliseconds=time.microsecond/1000)
-
-def extract_clip(sub, filename):
-    start = sub.start.to_time()
-    duration = (sub.end - sub.start).to_time()
-    extended = time_to_timedelta(duration) + datetime.timedelta(seconds=1)
-    if USE_REENCODING:
-        args = 'ffmpeg -i "{}" -ss {} -t {} -vcodec libx264 -async 1 {}'.format(VID_FILE, start, extended, filename)
-    else:
-        args = 'ffmpeg -i "{}" -ss {} -t {} -acodec copy -vcodec copy -async 1 {}'.format(VID_FILE, start, extended, filename)
-    # print args
-    subprocess.call(args)
-
-def get_extension(file_path):
-    extension = os.path.splitext(file_path)[1]
-    return extension
-
-def get_clip_names(subs, prefix=''):
-    extension = get_extension(VID_FILE)
-    return ['{}_{}{}'.format(prefix, sub.index, extension) for sub in subs]
-
-def extract_clips(subs, clip_names):
-    for sub, clip_name in zip(subs, clip_names):
-        extract_clip(sub, clip_name)
+    output_name = '{}_merge{}'.format(prefix, extension)
+    merge_clips(clip_names, output_name)
+    map(os.remove, clip_names)
 
 def get_ffmpeg_concact(file_names):
     lines = ["file '{}'".format(f) for f in file_names]
@@ -57,21 +30,44 @@ def merge_clips(clip_names, output_name):
     subprocess.call('ffmpeg -f concat -i {} -c copy {}'.format(concat, output_name))
     os.remove(concat)
 
-def srt_cut(prefix, pattern):
-    all_subs = get_subs()
-    subs_to_extract = filter_subs(all_subs, pattern)
-    print len(subs_to_extract), 'parts to extract'
-    if len(subs_to_extract) is 0:
-        return
-    clip_names = get_clip_names(subs_to_extract, prefix)
-    extract_clips(subs_to_extract, clip_names)
+def time_to_timedelta(time):
+    return datetime.timedelta(hours=time.hour, minutes=time.minute, seconds=time.second, milliseconds=time.microsecond/1000)
 
-    output_name = '{}_merge{}'.format(prefix, get_extension(VID_FILE))
-    merge_clips(clip_names, output_name)
-    map(os.remove, clip_names)
+def extract_clip(path_video, sub, filename, reencode):
+    start = time_to_timedelta(sub.start.to_time()) - datetime.timedelta(seconds=0.5)
+    duration = (sub.end - sub.start).to_time()
+    extended = time_to_timedelta(duration) + datetime.timedelta(seconds=0.5)
+    if reencode:
+        args = 'ffmpeg -i "{}" -ss {} -t {} -vcodec libx264 -async 1 {}'.format(path_video, start, extended, filename)
+    else:
+        args = 'ffmpeg -i "{}" -ss {} -t {} -acodec copy -vcodec copy -async 1 {}'.format(path_video, start, extended, filename)
+    # print args
+    subprocess.call(args)
+
+def extract_clips(path_video, subs, clip_names, reencode=True):
+    for sub, clip_name in zip(subs, clip_names):
+        extract_clip(path_video, sub, clip_name, reencode)
+
+def get_clip_names(subs, extension, prefix=''):
+    #extension = get_extension(VID_FILE)
+    return ['{}_{}{}'.format(prefix, sub.index, extension) for sub in subs]
+
+def filter_subs(subs, pattern):
+    ret = []
+    for sub in subs:
+        if re.search(pattern, sub.text, re.MULTILINE):
+            ret.append(sub)
+    return ret
+
+def get_extension(file_path):
+    extension = os.path.splitext(file_path)[1]
+    return extension
 
 def main():
-    srt_cut('prefix', '(r|R)egex')
+    path_video = 'DemoData/Sintel.2010.x264-VODO.mp4'
+    path_srt = 'DemoData/sintel_en.srt'
+    srt_movie_split(path_video, path_srt, 'scales', '(s|S)cales')
+    #srt_movie_split(path_video, path_srt, 'you', '(y|Y)ou')
 
 if __name__ == '__main__':
     main()
